f6ce0ff71a40c3580ed6e6dd60a7714a5ca078a4
Cloud User <centos@centos-g1-small3-su-1.novalocal>
2022-04-28 08:23:31 +0000

    initial commit

---
+++ src/main/java/Com/cloudifi/springtest/springtest/SpringtestApplication.java
@@ -0,0 +1,13 @@
+package Com.cloudifi.springtest.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Com/cloudifi/springtest/springtest/SpringtestApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.cloudifi.springtest.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
